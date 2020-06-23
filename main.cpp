fdf5f6b93fb2d304dbba351ee5669bc5bdc32c80
Dmitriy Nikiforov <d.nikiforov@ion.team>
2020-06-23 13:18:32 +0700

    added 'printHelpMessage' function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
-int main(){
-        std::cout << "hi\n";
-        return 0;
+void printHelpMessage() {
+	static std::string binary_name = "test";
+	std::cout << "Usage:\n"
+	          << "  " << binary_name << " -h                                - for output a help message\n"
+	          << "  " << binary_name << " -f <file_name> -m words -v <word> - for calculate count of words <word> in the file <file_name>\n"
+	          << "  " << binary_name << " -f <file_name> -m checksum        - for calculate a checksum of <file_name> file\n";
+}
+
+int main(int argc, char** argv){
+	printHelpMessage();
+    return 0;
 }
